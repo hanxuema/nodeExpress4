86d8db67934aca450f96149baf51d245e42a9730
Xavier Xie <xieyuehao@gmail.com>
2018-12-16 22:32:14 +1000

    Update bootstrap and jquery

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var debug = require('debug')('app');
+var express = require("express");
+var debug = require("debug")("app");
+var path = require("path");
+
 var app = express();
-var morgan  = require('morgan');
 
-app.use(morgan(''))
-app.get('/', function(req, res){
-    res.send('Hello from my app.')
-})
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css')))
 
-app.listen(3000, function () {
-    console.log("listening on port 3000");
-})
+app.use('/js', express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname,'/node_modules/jquery/dist')))
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(3000, function() {
+  console.log("listening on port 3000");
+});
